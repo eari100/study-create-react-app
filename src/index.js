3b8c06d07dcbb8e99f0c8ab60be501e7247ce374
Jaewook <eari100@naver.com>
2022-10-27 13:35:53 +0900

    feat: Hello World

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Chap1 from './Chap1-Hello-World'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Chap1 />
   </React.StrictMode>
 );
 
