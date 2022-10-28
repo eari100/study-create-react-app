36d45d53556cdcf1437172d6bbd80dc545b80b33
Jaewook <eari100@naver.com>
2022-10-28 16:07:20 +0900

    refactor: State and Lifecycle
    - FormattedDate 컴포넌트화

--- src/docs/Chap5-State-Life.js
+++ src/docs/Chap5-State-Life.js
@@ -7,6 +7,10 @@
 
 import React from 'react'
 
+const FormattedDate = props => (
+    <h2>It is {props.date.toLocaleTimeString()}.</h2>
+)
+
 class Clock extends React.Component {
     constructor(props) {
         super(props)
@@ -29,7 +33,7 @@ class Clock extends React.Component {
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <FormattedDate date={this.state.date} />
             </div>
         )
     }
